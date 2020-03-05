ab4d95dd838895fc03967e63f61f64859f0c5417
Julian de Jesus Lopez Lopez <julian.lz@ibm.com>
2020-03-04 18:37:04 -0600

    clear

--- loadImages.py
+++ loadImages.py
@@ -20,8 +20,6 @@ labels_ttl = ParseTripe.ParseTriples("resources/labels_en.ttl")
 while temp < 100:
     temp_images = images_ttl.getNext()
     if(temp_images[1] == "http://xmlns.com/foaf/0.1/depiction"):
-        temp_arr = [temp_images[0], temp_images[2]]
-        images.append(temp_arr)
         kv_images.put(str(temp_images[0]),str(temp_images[2]))
         if not temp_images[0] in temp_cat:
             temp_cat.append(temp_images[0])
@@ -36,15 +34,11 @@ while temp < 100:
             for label in len_labels:
                 if temp_labels[0] in temp_cat:
                     stemmed_word=Stemmer.stem(label)
-                    temp_arr = [stemmed_word,sort_value, temp_labels[0]]
-                    terms.append(temp_arr)
                     kv_labels.putSort(str(stemmed_word),str(sort_value),str(temp_labels[0]))
         else:
-            temp_arr = [temp_labels[2],sort_value,temp_labels[0]]
-            terms.append(temp_arr)
             kv_labels.putSort(str(stemmed_word),str(sort_value),str(temp_labels[0]))
     temp+=1
-    
+
 print("Labels processed")
 
 # Close KeyValues Storages
